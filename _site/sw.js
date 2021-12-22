584f0e0c97a689f3de263bc731bd64a0f0934c30
gominnam <kosok03@naver.com>
2021-12-23 00:58:06 +0900

    bundle update

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20211223.0026';
+const cacheName = 'chirpy-20211223.0056';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
